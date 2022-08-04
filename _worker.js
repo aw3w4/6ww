b7046054f7dd7b8678e90e30af5ff8fb1f5cd494
aw3w4 <103389916+aw3w4@users.noreply.github.com>
2022-08-04 03:30:40 +0000

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'huge6a.herokuapp.com'
+      url.hostname = 'key92h.herokuapp.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
